4f3f1ba830842228d354303ea533b5697f3e6a4f
Poorvi <desaipoorvi14@gmail.com>
2022-09-30 00:19:42 +0530

    first

---
+++ food/food_app/src/app/FoodProduct/add-food-product/add-food-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-food-product',
+  templateUrl: './add-food-product.component.html',
+  styleUrls: ['./add-food-product.component.css']
+})
+export class AddFoodProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ food/food_app/src/app/FoodProduct/view-food-product/view-food-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-food-product',
+  templateUrl: './view-food-product.component.html',
+  styleUrls: ['./view-food-product.component.css']
+})
+export class ViewFoodProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
